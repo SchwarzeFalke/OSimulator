0fce6e95ebc34e681f9344352c8b19eab5ccc62d
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-02-06 01:55:07 -0600

    Adding some comments to the documentation

--- Part 1/batch.py
+++ Part 1/batch.py
@@ -1,7 +1,7 @@
 # @Author: schwarze_falke
 # @Date:   2019-02-05T01:42:13-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-02-05T03:38:57-06:00
+# @Last modified time: 2019-02-06T00:07:10-06:00
 
 import time, sys, random, os, constant
 
@@ -13,21 +13,38 @@ class Batch:
     def addProcess(self, data):
         self.processes.append(data)
 
-    def execBatch(self):
+    def execBatch(self, totalTime):
+        counter = 3
+        secondsClock = 0
         for process in self.processes:
-            print("ID: {}".format(process.id))
-            print("Nombre del programador: {}".format(process.progName))
-            print("Tiempo maximo estimado: {}".format(process.maxTime))
-            print("Operacion: {} {} {}".format(process.op1, process.op, process.op2))
             process.realTime = random.randint(3,process.maxTime)
             time_start = time.time()
             seconds = 0
             while True:
                 try:
-                    print("Tiempo transcurrido: {}s | Tiempo restante: {}s".format(seconds, process.realTime-seconds), end="\r")
+                    exec = "P R O C E S O   E N   E J E C U C I O N"
+                    finish = "P R O C E S O S   T E R M I N A D O S"
+                    string = "ID: {:>4} | Nombre del programador: {:>10} | Tiempo maximo estimado: {:>2}s | Operacion: {} {} {} | Tiempo transcurrido: {:2}s | Tiempo restante: {:2}s".format(process.id, process.progName, process.maxTime, process.op1, process.op, process.op2, seconds, process.realTime-seconds)
+                    timeClock = int(totalTime + secondsClock)
+                    timeString = "Tiempo transcurrido total: {}s".format(timeClock)
+                    self.print_there(8, 50, exec)
+                    self.print_there(10, 6, string)
+                    self.print_there(15, 50, finish)
+                    self.print_there(2, 0, timeString)
                     time.sleep(1)
                     seconds += 1
-                    if seconds > process.realTime:
+                    secondsClock += 1
+                    if seconds >= process.realTime:
+                        proc = "ID: {:>4} | Nombre del programador: {:>10} | Tiempo de ejecucion: {:>2}s | Operacion y resultado: {} {} {} = {}".format(process.id, process.progName, process.realTime, process.op1, process.op, process.op2, process.result)
+                        x = 18 + ((self.id * 3)-counter)
+                        self.print_there(x, 6, proc)
+                        counter -= 1
                         break;
                 except e:
                     break
+        self.print_there(x, 130, "FIN DEL LOTE NO. {}".format(self.id))
+        return (timeClock+1)
+
+    def print_there(self, x, y, text):
+        sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+        sys.stdout.flush()
--- Part 1/script.py
+++ Part 1/script.py
@@ -1,33 +1,48 @@
 # @Author: schwarze_falke
 # @Date:   2019-02-05T01:42:08-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-02-05T03:32:39-06:00
+# @Last modified time: 2019-02-06T00:22:52-06:00
 
 import sys, os, constant, time
 from batch import Batch
 from task import Task
 
-TOTAL_BATCH = []
-ID_REGISTER = []
+TOTAL_BATCH = [] # Estructura principal para almacenar los lotes
+ID_REGISTER = [] # Estructura de datos para almacenar los ID y hacerlos irrepetibles
 
-def capture(amount):
-    count = 0
+# Función para imprimir en pantalla acorde a las coordenadas (x, y) parametrizadas
+def print_there(x, y, text):
+    sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+    sys.stdout.flush()
+
+# Función para capturar los procesos/tareas de forma secuencial iterativa
+def capture(amount):            # @amount es el parámetro de cantidad de procesos a capturar
+    count = 0                   # @count lleva la cuenta de lotes acumulados/capturados
+    idCount = 1                 # @idCount es una variable auxiliar para asignarle un ID consecutivo a los lotes
     while count < amount:
-        flag = 0
-        batch = Batch(count)
-        while flag < 3:
-            os.system(constant.CLEAR)
+        # Si la cantidad de procesos a capturar es menor a 3 (cantidad total que compone a un lote)
+        # la bandera de conteo se reduce para capturar lotes incompletos
+        if amount < 3:
+            flag = 3 - amount
+        else:
+            flag = 0
+        batch = Batch(idCount)                  # Se asigna un tipo de dato Batch con ID en su constructor
+        while flag < 3 and count < amount:
+            os.system(constant.CLEAR)           # Limpieza de pantalla
 
             while True:
                 id = input("Ingrese el ID del programa: ")
-                if (id in ID_REGISTER) == False:
-                    ID_REGISTER.append(id)
+                if (id in ID_REGISTER) == False:            # Si el ID capturado no se encuentra en el registro
+                    ID_REGISTER.append(id)                  # de ID, agrega el nuevo ID al registro
                     break
                 else:
-                    print("El ID se encuentra repetido!")
+                    print("El ID se encuentra repetido!")   # de lo contrario, el ID se encuentra repetido
 
             progName = input("Ingrese el nombre del programador: ")
 
+            # El tiempo máximo esperado debe ser mayor a 0,
+            # si no se cumple con este requisito, la iteración
+            # se repetirá hasta que la validación se haga verdadera y rompa el while
             while True:
                 try:
                     maxTime = int(input("Ingrese el tiempo maximo esperado: "))
@@ -36,8 +51,12 @@ def capture(amount):
                     else:
                         print("El tiempo maximo esperado debe ser mayor a 0!")
                 except:
-                    print("Ha ingresado un numero invalido!")
+                    print("Ha ingresado un numero invalido!")       # Las excepciones dadas por el input
+                                                                    # son de tipo int(); si se ingresa un caracter
+                                                                    #  alfanumérico, la excepción es atrapada
 
+                                                                    # Los siguientes inputs funcionan
+                                                                    # bajo la misma premisa de validación
             while True:
                 try:
                     op1 = int(input("Ingrese un numero entero: "))
@@ -60,22 +79,35 @@ def capture(amount):
                      print("No es un numero valido!")
 
             flag += 1
+            count += 1
+            # Se crea un proceso o tarea con la información capturada
             process = Task(progName, op1, op2, op, maxTime, id)
+            # Dicho proceso es añadido a la lista de procesos del lote
             batch.addProcess(process)
 
-        count += flag
+        idCount += 1
+        # El lote es añadido a la estructura de lotes
         TOTAL_BATCH.append(batch)
 
-
 def show(amount):
+    os.system(constant.CLEAR)
     count = 0
-    while count < amount:
-        print("Lote en ejecucion: {}".format(count+1))
-        print("Lotes restantes: {}".format(amount-(count+1)))
-        TOTAL_BATCH[count].execBatch()
-
+    top = int(amount/3)
+    if (amount % 3) != 0:
+        top += 1
+    totalTime = 0
+    while count < top:
+        string = "Lote en ejecucion: {}\t\t\tLotes restantes: {}".format((count+1), int(top-(count+1)))
+        print_there(3, 0, string)
+        totalTime = TOTAL_BATCH[count].execBatch(totalTime)
+        count += 1
+    end = "-----------------------------------------------------------TODOS LOS PROCESOS HAN SIDO TERMINADOS-----------------------------------------------------------"
+    timeString = "Tiempo transcurrido total: {}s".format(totalTime)
+    print_there(2, 0, timeString)
+    print_there(10, 6, end)
 
 def main():
+    os.system(constant.CLEAR)
     amount = int(input("Ingrese la cantidad de procesos: "))
     capture(amount)
     show(amount)
